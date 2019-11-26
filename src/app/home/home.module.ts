e5f536e59b6127c306202a75507a713a4428df14
Kevin Chambers <kevinchambers@Kevins-MacBook-Air.local>
2019-11-25 23:54:04 -0500

    Adding ratings stars, page link highlighting, checkboxes on the sidebar, dummied in carousel.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CarouselComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
